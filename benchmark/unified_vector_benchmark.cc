0617d6a9c776ce3dfb7f9b09be4ebbe7311de405
Thomas Hauth <Thomas.Hauth@kit.edu>
2017-12-22 19:45:11 +0100

    first version of visitor

--- benchmark/unified_vector_benchmark.cc
+++ benchmark/unified_vector_benchmark.cc
@@ -1,72 +1,102 @@
-
 #include <unified_vector.h>
 
-class DataClassA {};
+#include <functional>
+#include <iostream>
+#include <string>
 
-class DataClassB {};
+class DataClassA {
+public:
+	void dumpContent() {
+		std::cout << "This is class A" << std::endl;
+	}
+};
 
-class DataClassC {};
+class DataClassB {
+public:
+	void dumpContent() {
+		std::cout << "This is class B" << std::endl;
+	}
+};
 
+class DataClassC {
+public:
+	void dumpContent() {
+		std::cout << "This is class C" << std::endl;
+	}
+};
 
-class Test{ 
+class Test {
 public:
 
-    void push_back(DataClassA ) {};
+	void push_back(DataClassA) {
+	}
 
-    void push_back(DataClassB ) {};
+	void push_back(DataClassB) {
+	}
 
 };
 
-int main() {
+namespace {
 
-    unified_vector<DataClassA, DataClassB, DataClassC> uv;
-
-    uv.push_back(DataClassA());
-    uv.push_back(DataClassB());
-    uv.push_back(DataClassB());
-    uv.push_back(DataClassC());
-
-    return 0;
+template<class TUsedInLambda>
+void genLambdaFunction(TUsedInLambda fn) {
+	fn(23, 34);
 }
 
+}
+
+int main() {
+
+	unified_vector<DataClassA, DataClassB, DataClassC> uv;
+
+	uv.push_back(DataClassA());
+	uv.push_back(DataClassB());
+	uv.push_back(DataClassB());
+	uv.push_back(DataClassC());
+
+	auto lmd = [](auto & obj ) -> void { obj.dumpContent();};
+	uv.visit( lmd );
+
+	return 0;
+}
 
 /*
 
-#include <memory>
+ #include <memory>
 
-#include <cxxabi.h>
+ #include <cxxabi.h>
 
-using namespace std;
+ using namespace std;
 
-// GCC demangling -- not required for functionality
-string demangle(const char* mangled) {
-  int status;
-  unique_ptr<char[], void (*)(void*)> result(
-    abi::__cxa_demangle(mangled, 0, 0, &status), free);
-  return result.get() ? string(result.get()) : "ERROR";
-}
+ // GCC demangling -- not required for functionality
+ string demangle(const char* mangled) {
+ int status;
+ unique_ptr<char[], void (*)(void*)> result(
+ abi::__cxa_demangle(mangled, 0, 0, &status), free);
+ return result.get() ? string(result.get()) : "ERROR";
+ }
 
-template<typename Param>
-struct BaseSingle {
-  virtual void BaseFoo(Param) {
-    cout << "Hello from BaseSingle<"
-         << demangle(typeid(Param).name())
-         << ">::BaseFoo" << endl;
-  };
-};
+ template<typename Param>
+ struct BaseSingle {
+ virtual void BaseFoo(Param) {
+ cout << "Hello from BaseSingle<"
+ << demangle(typeid(Param).name())
+ << ">::BaseFoo" << endl;
+ };
+ };
 
-template<typename... Params>
-struct Base : public BaseSingle<Params>... {
-  template<typename T> void Foo(T&& x) {
-    this->BaseSingle<T>::BaseFoo(forward<T>(x));
-  }
-};
+ template<typename... Params>
+ struct Base : public BaseSingle<Params>... {
+ template<typename T> void Foo(T&& x) {
+ this->BaseSingle<T>::BaseFoo(forward<T>(x));
+ }
+ };
 
-int main() {
-  Base<string, int, bool> b;
-  b.Foo(1);
-  b.Foo(true);
-  b.Foo(string("ab"));
-}
+ int main() {
+ Base<string, int, bool> b;
+ b.Foo(1);
+ b.Foo(true);
+ b.Foo(string("ab"));
+ }
 
-*/
+ */
--- unified_vector/unified_vector.h
+++ unified_vector/unified_vector.h
@@ -1,46 +1,76 @@
-
+#pragma once
 
 /*
-template <class... Ts> struct tuple {};
+ template <class... Ts> struct tuple {};
 
-template <class T, class... Ts>
-struct tuple<T, Ts...> : tuple<Ts...> {
-  tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {}
+ template <class T, class... Ts>
+ struct tuple<T, Ts...> : tuple<Ts...> {
+ tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {}
 
-  T tail;
-};
-*/
+ T tail;
+ };
+ */
 
 #include <vector>
+#include <functional>
 
-template <class ... Ts > class unified_vector{};
-
-template <class T>
-class unified_vector<T> { 
-public:
-    void push_back( T const& item ) {
-        m_items.emplace_back( item );
-    }
-
-private:
-    std::vector<T> m_items;
-};
-
-template <class T, class... Ts>
-class unified_vector<T, Ts...> : public unified_vector<Ts...> { 
+template<class TForType>
+class unified_vector_implement_for_type {
 public:
 
-    // using is required when overloading a method in a derived class
-    // otherwise, the compiler will stop looking for methods once one
-    // (even with wrong signature) with the correct mothed name is found
-    using unified_vector<Ts...>::push_back;
+	typedef std::function<void(TForType & item)> visiting_type;
 
-    void push_back( T const& item ) {
-        m_items.emplace_back( item );
-    }
+	void push_back(TForType const& item) {
+		m_items.emplace_back(item);
+	}
+
+	template<class TVisitor>
+	void _internal_visit(TVisitor visitor) {
+		for (auto item : m_items) {
+			visitor(item);
+		}
+	}
 
 private:
-    std::vector<T> m_items;
+	std::vector<TForType> m_items;
 };
 
+template<class ... Ts> class unified_vector {
+};
+
+template<class T>
+class unified_vector<T> : public unified_vector_implement_for_type<T> {
+public:
+	using unified_vector_implement_for_type<T>::push_back;
+
+	template<class TVisitor>
+	void visit(TVisitor visitor) {
+		unified_vector_implement_for_type<T>::_internal_visit(visitor);
+
+		// not recursive call here, obviously
+	}
+};
+
+template<class T, class ... Ts>
+class unified_vector<T, Ts...> : public unified_vector<Ts...>,
+		unified_vector_implement_for_type<T> {
+public:
+
+	// using is required when overloading a method in a derived class
+	// otherwise, the compiler will stop looking for methods once one
+	// (even with wrong signature) with the correct mothed name is found
+	using unified_vector<Ts...>::push_back;
+	using unified_vector_implement_for_type<T>::push_back;
+
+	template<class TVisitor>
+	void visit(TVisitor visitor) {
+		unified_vector_implement_for_type<T>::_internal_visit(visitor);
+
+		// recurse the visitor for all other types in the unified vector
+		unified_vector<Ts...>::visit(visitor);
+	}
+	/*
+
+	 */
+};
 
