3e541d29bbba411a2adc05bc8128de3ba5fb3393
Thomas Hauth <Thomas.Hauth@kit.edu>
2018-07-18 17:28:24 +0900

    removed unused example

--- unified_vector/unified_vector.h
+++ unified_vector/unified_vector.h
@@ -1,17 +1,5 @@
 #pragma once
 
-/*
- template <class... Ts> struct tuple {};
-
- template <class T, class... Ts>
- struct tuple<T, Ts...> : tuple<Ts...> {
- tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {}
-
- T tail;
- };
- */
-
-
 #include <vector>
 #include <functional>
 #include <algorithm>
